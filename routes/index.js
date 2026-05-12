60a2fa48fed722244511ae94df741d3bfe16b7de
vamundse <vamundse@gmail.com>
2026-05-12 19:09:31 +0200

    changed the message on the front page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello, see the contact data by going to /contacts.')
+    res.send('Hello World. Go to /api-docs to see how it works.')
 });
 
 router.use('/contacts', require('./contacts'));
