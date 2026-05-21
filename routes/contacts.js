4470c0ac4dfccd511b273c01b60ee28ebab1fd49
vamundse <vamundse@gmail.com>
2026-05-21 11:32:13 +0200

    Have added the spreader to the validation rules

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,8 +5,8 @@ const validator = require('../utilities/userValidation');
 router.get('/', contactsController.getAll);
 router.get('/:id', contactsController.getSingle);
 
-router.post('/', validator.contactValidationRules(), validator.validate, contactsController.createContact);
-router.put('/:id', validator.contactValidationRules(), validator.validate, contactsController.updateContact);
+router.post('/', ...validator.contactValidationRules(), validator.validate, contactsController.createContact);
+router.put('/:id', ...validator.contactValidationRules(), validator.validate, contactsController.updateContact);
 router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
