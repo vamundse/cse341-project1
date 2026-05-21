8ff4366513e6ad403b111e51197e95f77a3373aa
vamundse <vamundse@gmail.com>
2026-05-21 12:45:04 +0200

    made fixes to the error handler and the routes affected by it

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const contactsController = require('../controllers/contacts');
 const validator = require('../utilities/contactValidation');
+const errorHandler = require('../utilities/errorHandler')
 
-router.get('/', contactsController.getAll);
-router.get('/:id', contactsController.getSingle);
+router.get('/', errorHandler(contactsController.getAll));
+router.get('/:id', errorHandler(contactsController.getSingle));
 
-router.post('/', ...validator.contactValidationRules(), validator.validate, contactsController.createContact);
-router.put('/:id', ...validator.contactValidationRules(), validator.validate, contactsController.updateContact);
-router.delete('/:id', contactsController.deleteContact);
+router.post('/', ...validator.contactValidationRules(), validator.validate, errorHandler(contactsController.createContact));
+router.put('/:id', ...validator.contactValidationRules(), validator.validate, errorHandler(contactsController.updateContact));
+router.delete('/:id', errorHandler(contactsController.deleteContact));
 
 module.exports = router;
---
+++ utilities/errorHandler.js
@@ -0,0 +1,3 @@
+const errorHandler = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
+
+module.exports = { errorHandler };
