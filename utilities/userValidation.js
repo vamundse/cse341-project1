5a5ede424c9701c06dcb913eb34b2a99cabd2f93
vamundse <vamundse@gmail.com>
2026-05-21 11:45:18 +0200

    changed the error message

--- utilities/userValidation.js
+++ utilities/userValidation.js
@@ -34,7 +34,7 @@ const validate = (req, res, next) => {
     console.log('Full error object:', JSON.stringify(errors.array(), null, 2)); // Debug
 
     const extractedErrors = []
-    errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }));
+    errors.array().map(err => extractedErrors.push({ [err.path]: err.msg }));
     
     return res.status(422).json({
         errors: extractedErrors,
